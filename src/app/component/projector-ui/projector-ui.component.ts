596d0bc0c2bacc66b05bec3648400b64f3597628
DulanjayaChathura <sacdulanjaya.16@cse.mrt.ac.lk>
2020-10-24 01:20:52 +0530

    BUGFIX: two verb sentences

--- src/app/component/projector-ui/projector-ui.component.ts
+++ src/app/component/projector-ui/projector-ui.component.ts
@@ -11,9 +11,13 @@ import {ProjectorServiceService} from '../../service/projector-service.service';
 })
 export class ProjectorUIComponent implements OnInit {
   output;
-  words = [];
-  labels = [];
+  word='';
+  roleList = [];
+  argList = [];
+  keyList = [];
+  verbList=[];
   loading= false;
+  roles =[]
 
   formGroup = new FormGroup({
     english: new FormControl(),
@@ -24,23 +28,58 @@ export class ProjectorUIComponent implements OnInit {
 
   }
   submit() {
-    this.words = [];
-    this.labels = [];
+    this.roles = [];
+    this.keyList=[];
+    this.verbList=[];
     this.loading = true;
     this.projectorService.request(this.formGroup.get('english').value, this.formGroup.get('sinhala').value).subscribe(response => {
       this.output =  this.extractor(JSON.parse("[" + response["result"] + "]"));
 
+
     });
   }
   extractor(respone) {
+    // console.log(respone)
     if(respone != null) {
       this.loading = false;
     }
     for (var val of respone[0]) {
-       this.words.push(val["text"].trim());
-       this.labels.push(val["frame"].trim().replace("]", "").replace("[B-", "").replace("[I-", "").replace("[O-", "").replace("[", ""));
+       this.roleList = [];
+       this.word=val["text"]
+       if(this.word=="*"){
+         continue
+       }
+
+       // console.log(val["frame"].replace("[", "").replace("]", "").split(", "))
+
+       this.keyList.push(this.word);
+       this.argList = val["frame"].replace("[", "").replace("]", "").split(", ")
+      // console.log(this.argList)
+
+       for (var val of this.argList) {
+          // console.log(val)
+          //  console.log(val.search("."))
+//          console.log(val.includes("."))
+          if (val.includes(".")){
+            this.roleList.push(val)
+            this.verbList.push(val)
+//            console.log(val)
+          }else if(val.includes("ARG")){
+            // console.log(val.substring(2))
+            this.roleList.push(val.substring(2))
+          }else{
+            this.roleList.push("__")
+          }
+
+
+       }
+      this.roles.push(this.roleList)
+       // this.labels.push(val["frame"].replace("]", "").replace("[B-", "").replace("[I-", "").replace("[O-", "").replace("[", ""));
 
     }
+     console.log(this.roles)
+    console.log(this.keyList)
+    console.log(this.verbList)
   }
 
 }
